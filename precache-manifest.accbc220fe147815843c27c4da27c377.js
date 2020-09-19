c517dd1b794a30fa5e2dabc5d8c909971bf321ab
Timothy <timothyroy2011@gmail.com>
2020-09-19 18:52:44 +0600

    Updates

---
+++ precache-manifest.accbc220fe147815843c27c4da27c377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f0db99085b1f10781264277fa3544e",
+    "url": "/quiz-app.git/index.html"
+  },
+  {
+    "revision": "44bdb3089f8d8c65afb3",
+    "url": "/quiz-app.git/static/js/2.6d192f3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app.git/static/js/2.6d192f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb584c496a37ce79d3ab",
+    "url": "/quiz-app.git/static/js/main.9fa01fbb.chunk.js"
+  },
+  {
+    "revision": "9beff78eb016b30ebecb",
+    "url": "/quiz-app.git/static/js/runtime-main.c3124526.js"
+  },
+  {
+    "revision": "0c39f498e31bd99d333bcb0214434287",
+    "url": "/quiz-app.git/static/media/background.0c39f498.jpg"
+  }
+]);
